9a570efc9e14861a26bda30defa23c18ee741ab8
Jaime Guzmán <me@jguzman.cl>
2024-11-24 03:28:05 -0300

    bkp

--- apps/frontend-money/pages/index.js
+++ apps/frontend-money/pages/index.js
@@ -26,9 +26,9 @@ export default function Home() {
 
         <KPISection />
 
-        <div className="mt-8 space-y-6">
+        {/* <div className="mt-8 space-y-6">
           <PaymentsInformation />
-        </div>
+        </div> */}
         
         <div className="grid grid-cols-1 gap-4 mt-4 md:grid-cols-2">
           <CandidatesAttention />
